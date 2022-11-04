83724eb04b912feefad966fe5ab7d51d28493cad
Erikka Polk <Erikka@Erikkas-MacBook-Pro.local>
2022-11-04 17:43:46 -0500

    finished assignment w/ pp partner

--- arithmetic.py
+++ arithmetic.py
@@ -1,35 +1,47 @@
 """Functions for common math operations."""
 
+#https://fellowship.hackbrightacademy.com/materials/wmt5/exercises/calculator-1/solution/index.html
 
 def add(num1, num2):
-    """Return the sum of num1 and num2."""
-
-    return 10
-
-
+    """Return the sum of num1 and num2."""  #Enter your equation > + 1 2  -> 3.0
+    return num1 + num2
+   
 def subtract(num1, num2):
     """Return the value of num1 minus num2."""
-
+    return num1 - num2         
 
 def multiply(num1, num2):
-    """Multiply the num1 by num2 and return the result."""
-
+    """Multiply the num1 by num2 and return the result."""    
+    return num1 * num2
 
 def divide(num1, num2):
     """Divide the num1 by num2, returning a floating point."""
-
+    return num1 / num2
 
 def square(num1):
     """Return the square of num1."""
+    return num1 * num1
 
-
-def cube(num1):
+def cube(num1):         
     """Return the cube of num1."""
-
+    return num1 * num1 * num1
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
-
+    return num1 ** num2
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
+    return num1 % num2
+
+# prefix notation
+
+#new-
+
+def add_mult(num1, num2, num3):
+    """Multiply the num1 by num2 and return the result."""    
+    return num1 *+ num2 *+ num3
+
+def add_cubes(num1, num2):
+    """Multiply the num1 by num2 and return the result."""    
+    return num1 * num1 + num2
--- calculator.py
+++ calculator.py
@@ -1,37 +1,56 @@
 """CLI application for a prefix-notation calculator."""
 
 from arithmetic import *
+#the asterisk imports functions from arithmetic
 
+
+#https://fellowship.hackbrightacademy.com/materials/wmt5/exercises/calculator-1/solution/index.html
+
+
+
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#
+#the program is EXPLICITLY prefix arithmetic bc the conditionals are set to expect the token 
+# list item at index 0 to be the operator string....
+# operator string is designed here to dictate the function. if i enter "1 + 1" is gives error 
+# bc token[0] is a number instead of a operaor string as explicitly stated
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#
+
+
+#these "inputs" are almost like arguments....
+
+#why is this API built all funky like this? 
+#if -break-, elif -continue-
+#if, else, if, if not -continue-, elif, elif, elif, elif, elif, elif, else
 
 while True:
-    user_input = input("Enter your equation > ")
-    tokens = user_input.split(" ")
+    user_input = input("Enter your equation > ") #assigned input to var
+    tokens = user_input.split(" ")               #reassigned input to variable thatll return a seperated list of string inputs
 
-    if "q" in tokens:
-        print("You will exit.")
+    if "q" in tokens:                           #these inputs seem to be player controls almost, q= quit
+        print("You will exit.")                 #if q is in list of inputs
         break
 
-    elif len(tokens) < 2:
+    elif len(tokens) < 2:                       # if amount (in length) of inputs are way too little
         print("Not enough inputs.")
         continue
 
-    operator = tokens[0]
-    num1 = tokens[1]
+    operator = tokens[0]                        # variable for first input via index list
+    num1 = tokens[1]                            # variable for second input via index list
 
-    if len(tokens) < 3:
+    if len(tokens) < 3:                         # if amount (in length) of inputs are too little
         num2 = "0"
 
     else:
         num2 = tokens[2]
 
-    if len(tokens) > 3:
-        num3 = tokens[3]
+    if len(tokens) > 3:                          #need num3 as a parameter in function def
+        num3 = tokens[3]                       
 
     # A place to store the return value of the math function we call,
     # to give us one clear place where that result is printed.
     result = None
 
-    if not num1.isdigit() or not num2.isdigit():
+    if not num1.isdigit() or not num2.isdigit():        #if num1 or num2 is not a int
         print("Those aren't numbers!")
         continue
 
@@ -63,10 +82,10 @@ while True:
     elif operator == "mod":
         result = mod(float(num1), float(num2))
 
-    elif operator == "x+":
+    elif operator == "x+":                                              ##need function def
         result = add_mult(float(num1), float(num2), float(num3))
 
-    elif operator == "cubes+":
+    elif operator == "cubes+":                                          #need function def
         result = add_cubes(float(num1), float(num2))
 
     else:
